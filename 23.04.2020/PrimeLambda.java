767a1daa71aaf3083679a4e9ed89dd432113d542
Pratyush4456 <60208775+Pratyush4456@users.noreply.github.com>
2020-04-23 14:38:15 +0530

    Create PrimeLambda.java

---
+++ 23.04.2020/PrimeLambda.java
@@ -0,0 +1,19 @@
+package com.vp;
+public class PrimeLambda {
+
+	public static void main(String[] args) {
+
+	Even prime = (int a) -> {
+		for (int i = 2; i <=Math.sqrt(a); i++) {
+			   if(a%i==0) {
+				   System.out.println("false");
+			  }
+			   else {
+		  System.out.println("true");
+			   }
+		}
+	};
+	 prime.check(121);
+	 prime.check(1919);
+}
+}
