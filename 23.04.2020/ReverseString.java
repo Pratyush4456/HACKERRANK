5047fc29ac8ddf11260f2f917ecbdc18c469f0c3
Pratyush4456 <60208775+Pratyush4456@users.noreply.github.com>
2020-04-23 14:41:27 +0530

    Create ReverseString.java

---
+++ 23.04.2020/ReverseString.java
@@ -0,0 +1,22 @@
+package com.vp;
+public class ReverseString {
+
+		public static void main(String[] args) {
+		
+		String s1= "java class";
+		String s2= "cognizant training";
+		
+		Reverse rev=(s) -> {
+			
+			
+			String temp ="";
+			for(int i = s.length()-1;i>=0;i--) {
+
+				temp+=s.charAt(i);
+				}
+		return temp;
+		};
+	System.out.println(rev.reversestring(s1));
+  System.out.println(rev.reversestring(s2));
+}
+}
